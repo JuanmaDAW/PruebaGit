0683aaa362b8a8fb268bd1baaccd5340dd0bcff0
Felix Sabugal <fsabugalc03@educarex.es>
2024-06-05 19:13:29 +0000

    cambios en proyecto

--- src/proyecto/proyecto.java
+++ src/proyecto/proyecto.java
@@ -4,15 +4,7 @@ public class proyecto {
 
 	public static void main(String[] args) {
 		System.out.println("porfavor te lo pido");
-		System.out.println("porfavor te lo pido");
-		System.out.println("porfavor te lo pido");
-		System.out.println("porfavor te lo pido");
-		System.out.println("porfavor te lo pido");
-		System.out.println("porfavor te lo pido");
-		System.out.println("porfavor te lo pido");
-		System.out.println("porfavor te lo pido");
-		System.out.println("porfavor te lo pido");
-		System.out.println("porfavor te lo pido");
+
 	}
 
 }
