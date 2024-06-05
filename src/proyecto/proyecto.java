a35ab862b98466f2d1fe38842ec2f4fea7fea23c
fsabu <fsabu@DESKTOP-T4EQUBV>
2024-06-05 20:42:41 +0200

    juanma

---
+++ src/proyecto/proyecto.java
@@ -0,0 +1,18 @@
+package proyecto;
+
+public class proyecto {
+
+	public static void main(String[] args) {
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+	}
+
+}
